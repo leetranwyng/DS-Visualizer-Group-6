fc8762888122aca74c60da631465bca325fc0702
tetuongen <huynhnhatanh362@gmail.com>
2026-04-17 14:56:50 +0700

    delete excessive functions

--- src/structures/tree/AVLRenderer.cpp
+++ src/structures/tree/AVLRenderer.cpp
@@ -1,3 +1,4 @@
+#include "AVL.h"
 #include "AVLRenderer.h"
 #include "../../UI/UI.h"
 
@@ -131,48 +132,6 @@ void AVLRender::draw() {
         int textWidth = MeasureText(text.c_str(), fontSize);
 
         DrawText(text.c_str(), n.current.x - textWidth / 2, n.current.y - fontSize / 2, fontSize, WHITE);
-    }
-}
-
-void AVLRender::handleEvent(EventType type, TreeNode* key) {
-    events.push_back({type, key});
-}
-
-void AVLRender::animation(float time) {
-    if (!isVisualizing) return;
-
-    if (step >= events.size()){
-        isVisualizing = false;
-        return;
-    }
-
-    stepTimer += time;
-    if (stepTimer < 0.5f) return;
-    stepTimer = 0;
-    AVLevent e = events[step];
-    switch (e.type) {
-        case VISIT:
-            break;
-        case INSERT:
-            NodeShape n;
-            n.node = e.nodeVal;
-            n.current = {100, 100};
-            n.target = {100, 100};
-            n.radius = 20;
-            n.color = PINK;
-            nodes.push_back(n);
-            break;
-        case ROTATE_LEFT:
-        case ROTATE_RIGHT:
-            break;
-    }
-    step++;
-}
-
-void AVLRender::draw() {
-    for (auto &n : nodes) {
-        DrawCircleV(n.current, n.radius, n.color);
-        DrawText(TextFormat("%d", n.node->value), n.current.x - 10, n.current.y - 10, 20, WHITE);
     }
 }
 
